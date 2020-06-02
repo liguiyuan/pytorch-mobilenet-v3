c2102ef94813843315f0906b750029934d077ca8
liguiyuan <820001401@qq.com>
2020-06-02 14:55:21 +0800

    fixed some version error

--- mobilenetv3.py
+++ mobilenetv3.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from thop import profile
 
 __all__ = ['MobileNetV3', 'mobilenetv3']
 
@@ -235,16 +235,13 @@ if __name__ == '__main__':
     net = mobilenetv3()
     print('mobilenetv3:\n', net)
     print('Total params: %.2fM' % (sum(p.numel() for p in net.parameters())/1000000.0))
-    input_size=(1, 3, 224, 224)
-    # pip install --upgrade git+https://github.com/kuan-wang/pytorch-OpCounter.git
-    from thop import profile
-    flops, params = profile(net, input_size=input_size)
-    # print(flops)
-    # print(params)
+
+    inputs = torch.randn(1, 3, 224, 224)
+    flops, params = profile(net, inputs=(inputs, ))
+
     print('Total params: %.2fM' % (params/1000000.0))
     print('Total flops: %.2fM' % (flops/1000000.0))
-    x = torch.randn(input_size)
-    out = net(x)
+    out = net(inputs)
 
 
 
